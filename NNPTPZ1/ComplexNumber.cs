b6ca1ee9b94474575c24039405a68a6020bb88e2
Klára Kahounová <kahounova.klarka@seznam.cz>
2025-10-03 16:47:08 +0200

    třída ComplexNumber + třída Polynome - refaktor

---
+++ NNPTPZ1/ComplexNumber.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace NNPTPZ1
+{
+
+    namespace Mathematics
+    {
+        public class ComplexNumber
+        {
+            public double RealPart { get; set; }
+            public double ImaginaryPart { get; set; }
+
+            public static readonly ComplexNumber Zero = new ComplexNumber();
+
+            public override bool Equals(object obj) =>
+                obj is ComplexNumber number &&
+                RealPart == number.RealPart &&
+                ImaginaryPart == number.ImaginaryPart;
+
+            public ComplexNumber Multiply(ComplexNumber secondFactor) =>
+
+                new ComplexNumber
+                {
+                    RealPart = RealPart * secondFactor.RealPart - ImaginaryPart * secondFactor.ImaginaryPart,
+                    ImaginaryPart = RealPart * secondFactor.ImaginaryPart + ImaginaryPart * secondFactor.RealPart
+                };
+
+            public double GetAbsoluteValue() =>
+                Math.Sqrt(RealPart * RealPart + ImaginaryPart * ImaginaryPart);
+
+            public ComplexNumber Add(ComplexNumber secondFactor) =>
+                new ComplexNumber
+                {
+                    RealPart = RealPart + secondFactor.RealPart,
+                    ImaginaryPart = ImaginaryPart + secondFactor.ImaginaryPart
+                };
+            public double GetAngleInRadians() =>
+
+                Math.Atan(ImaginaryPart / RealPart);
+
+            public ComplexNumber Subtract(ComplexNumber secondFactor) =>
+                new ComplexNumber
+                {
+                    RealPart = RealPart - secondFactor.RealPart,
+                    ImaginaryPart = ImaginaryPart - secondFactor.ImaginaryPart
+                };
+
+            public override string ToString() =>
+
+              $"({RealPart} + {ImaginaryPart}i)";
+
+
+            public ComplexNumber Divide(ComplexNumber other)
+            {
+                ComplexNumber divident = Multiply(new ComplexNumber
+                {
+                    RealPart = other.RealPart,
+                    ImaginaryPart = -other.ImaginaryPart
+                });
+
+                double divisor = other.RealPart * other.RealPart + other.ImaginaryPart * other.ImaginaryPart;
+
+                return new ComplexNumber
+                {
+                    RealPart = divident.RealPart / divisor,
+                    ImaginaryPart = divident.ImaginaryPart / divisor
+                };
+            }
+
+        }
+    }
+}
---
+++ NNPTPZ1/Polynome.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+
+namespace NNPTPZ1
+{
+
+    namespace Mathematics
+    {
+        public class Polynome
+        {
+            /// <summary>
+            /// Coe
+            /// </summary>
+            public List<ComplexNumber> Coefficients { get; set; }
+
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            public Polynome() => Coefficients = new List<ComplexNumber>();
+
+            public void Add(ComplexNumber coefficient) =>
+                Coefficients.Add(coefficient);
+
+            /// <summary>
+            /// Derives this polynomial and creates new one
+            /// </summary>
+            /// <returns>Derivated polynomial</returns>
+            public Polynome Derive()
+            {
+                Polynome derivativePolynome = new Polynome();
+                for (int i = 1; i < Coefficients.Count; i++)
+                {
+                    derivativePolynome.Coefficients.Add(Coefficients[i].Multiply(new ComplexNumber() { RealPart = i }));
+                }
+
+                return derivativePolynome;
+            }
+
+            /// <summary>
+            /// Evaluates polynomial at given point
+            /// </summary>
+            /// <param name="x">point of evaluation</param>
+            /// <returns>y</returns>
+            public ComplexNumber Eval(double point) =>
+                Eval(new ComplexNumber { RealPart = point, ImaginaryPart = 0 });
+
+            /// <summary>
+            /// Evaluates polynomial at given point
+            /// </summary>
+            /// <param name="x">point of evaluation</param>
+            /// <returns>y</returns>
+            public ComplexNumber Eval(ComplexNumber point)
+            {
+                ComplexNumber polynomialValue = ComplexNumber.Zero;
+
+                for (int i = 0; i < Coefficients.Count; i++)
+                {
+                    ComplexNumber currentCoefficient = Coefficients[i];
+                    ComplexNumber termValue = currentCoefficient;
+
+                    if (i > 0)
+                    {
+                        ComplexNumber pointPower = point;
+                        for (int j = 0; j < i - 1; j++)
+                            pointPower = pointPower.Multiply(point);
+
+                        termValue = termValue.Multiply(pointPower);
+                    }
+
+                    polynomialValue = polynomialValue.Add(termValue);
+                }
+
+                return polynomialValue;
+            }
+
+
+            /// <summary>
+            /// ToString
+            /// </summary>
+            /// <returns>String repr of polynomial</returns>
+            public override string ToString()
+            {
+                string polynomialString = "";
+
+                for (int termIndex = 0; termIndex < Coefficients.Count; termIndex++)
+                {
+                    polynomialString += Coefficients[termIndex];
+
+                    if (termIndex > 0)
+                    {
+                        for (int powerIndex = 0; powerIndex < termIndex; powerIndex++)
+                        {
+                            polynomialString += "x";
+                        }
+                    }
+
+                    if (termIndex + 1 < Coefficients.Count)
+                        polynomialString += " + ";
+                }
+
+                return polynomialString;
+            }
+        }
+    }
+}
